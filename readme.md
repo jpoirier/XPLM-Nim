c401bf980498db793f1903392c0fa53fcfe02910
Joseph Poirier <jdpoirier@gmail.com>
2014-12-29 18:14:57 -0600

    add xplm lib file, readme update, set src path in example cfg files

--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 Based on X-Plane SDK Release 2.1.3 11/14/13 (http://www.xsquawkbox.net/xpsdk/).
 
 
-####  Build the CommViewer plugin
+####  Build the CommViewer plugin example
 $ nim c -d:release -d:useRealtimeGC -o:CommViewer.xpl CommViewer.nim
 
 Copy CommViewer.xpl to your X-Plane plugin folder, X-Plane10/Resources/plugins.
@@ -13,7 +13,7 @@ CommViewer Example (COMx: 0=Unselected, 1=Selected, TX/RX: ON=1, OFF=0):
 ![Alt text](./images/CommViewer.png "X-Plane screenshot")
 
 
-#### Build the plugin example
+#### Build the XPLM_Test plugin example
 $ nim c -d:release -d:useRealtimeGC -o:XPLM_Test.xpl XPLM_Test.nim
 
 Copy XPLM_Test.xpl to your X-Plane plugin folder, X-Plane10/Resources/plugins.
---
+++ src/xplm.nim
@@ -0,0 +1,46 @@
+# See license.txt for usage.
+
+when defined(windows):
+    const xplm_lib* = "./Resources/plugins/XPLM_64.dll"
+    const xpwidgets_lib* = "./Resources/plugins/XPWidgets_64.dll"
+elif defined(macosx):
+    const xplm_lib* = "./Resources/plugins/XPLM_64.dylib"
+    const xpwidgets_lib* = "./Resources/plugins/XPWidgets_64.dylib"
+else:
+    const xplm_lib* = "./Resources/plugins/XPLM_64.so"
+    const xpwidgets_lib* = "./Resources/plugins/XPWidgets_64.so"
+
+include "XPLMDefs.nim"
+include "XPWidgetDefs.nim"
+include "XPStandardWidgets.nim"
+
+include "XPLMCamera.nim"
+include "XPLMDataAccess.nim"
+include "XPLMDisplay.nim"
+include "XPLMGraphics.nim"
+include "XPLMMenus.nim"
+include "XPLMNavigation.nim"
+include "XPLMPlanes.nim"
+include "XPLMPlugin.nim"
+include "XPLMProcessing.nim"
+include "XPLMScenery.nim"
+include "XPLMUtilities.nim"
+include "XPUIGraphics.nim"
+include "XPWidgets.nim"
+include "XPWidgetUtils.nim"
+
+# import XPLMCamera
+# import XPLMDataAccess
+# import XPLMDisplay
+# import XPLMGraphics
+# import XPLMMenus
+# import XPLMNavigation
+# import XPLMPlanes
+# import XPLMPlugin
+# import XPLMProcessing
+# import XPLMScenery
+# import XPLMUtilities
+# import XPUIGraphics
+# import XPStandardWidgets
+# import XPWidgets
+# import XPWidgetUtils
